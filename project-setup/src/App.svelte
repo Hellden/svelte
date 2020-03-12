963084080ad0ff09894169f0360a7254809324f3
Hellden <r.chapotin@gmail.com>
2020-03-12 14:01:26 +0100

    add project 2

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,11 +1,21 @@
 <script>
-	export let name;
+  let course = "";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  .current {
+    color: red;
+  }
+
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Practice exercice</h1>
+
+<label for="">Couses goal</label>
+<input type="text" bind:value={course} />
+
+<h1 class:current={!course == ''}>{course}</h1>
